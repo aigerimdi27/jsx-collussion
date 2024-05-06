0d9ff7e2fc89e9be8bad21b83b0f721cd6fab6f4
aigerimdi27 <dzhetigenovaa@gmail.com>
2024-05-06 19:05:57 +0600

    finished the layout of header and hero

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import logo from '../assets/images/YOUR LOGO.png'
+import searchHeartCart from '../assets/images/search HEART shopping-cart.png'
+export const Header = () => {
+    return (
+        <header className="header" id="header">
+            <div className="container">
+                <img src={ logo } alt="" className='header__logo'/>
+                <nav className="header__menu">
+                    <a href="#" className="header__link">home</a>
+                    <a href="#" className="header__link">shop</a>
+                    <a href="#" className="header__link">lookbook</a>
+                    <a href="#" className="header__link">features</a>
+                    <a href="#" className="header__link">pages</a>
+                    <a href="#" className="header__link">blog</a>
+                </nav>
+                <img src={ searchHeartCart } alt="" className="header__nav" />
+            </div>
+        </header>
+    )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+export const Hero = () => {
+    return (
+        <section className="hero" id="hero">
+                <div className="hero__content">
+                    <p className="hero__text">NEW TREND</p>
+                    <h2 className="hero__title">COLLUSION</h2>
+                    <p className="hero__text">An exclusive selection of this season's trends.</p>
+                    <button className="hero__btn">DISCOVER</button>
+                    <button className="hero__btn">SHOP NOW</button>
+                </div>
+        </section>
+    )
+}
